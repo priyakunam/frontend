9b860a042d59d20685bc23c96d884eec487f68cd
abc <priyakunam97@gmail.com>
2023-03-16 15:25:29 +0530

    commit

--- version.sh
+++ version.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
-version="$1"
-major=0
-minor=0
-build=0
-# break down the version number into it's components
-regex="([0-9]+).([0-9]+).([0-9]+)"
-if [[ $version =~ $regex ]]; then
-  major="${BASH_REMATCH[1]}"
-  minor="${BASH_REMATCH[2]}"
-  build="${BASH_REMATCH[3]}"
-fi
-git for-each-ref --shell --format="ref=%(refname:short) dt=%(taggerdate:format:%s)" "refs/tags/*"
+# currentVersion="v1.4"
+currentVersion="v1.4"
+now=$(date +"%d%m%Y")
+# now=$(ref=%(refname:short) dt=%(taggerdate:format:%s)" "refs/tags/*)
+tagname="$currentVersion"_"$now"
+echo $tagname
+git checkout prod
+git config credential.helper store
+git stash
+git pull origin prod
+git tag $tagname -m "new release $tagname"
+git push origin $tagname
 
-
-# check paramater to see which number to increment
-if [[ "$2" == "feature" ]]; then
-	minor=$(echo $((minor + 1)))
-elif [[ "$2" == "bug" ]]; then
-	build=$(echo $((build + 1)))
-elif [[ "$2" == "major" ]]; then
-	major=$(echo $((major+1)))
-else
-  echo "usage: ./version.sh version_number [major/feature/bug]"
-  exit -1
-fi
-# echo the new version number
-echo "new version: ${major}.${minor}.${build}"
